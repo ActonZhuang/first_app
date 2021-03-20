13144be88b7b46f84006d2d65cad5abcfb453dae
zhuangtai <tai.zhuang@mvtrail.com>
2021-03-20 22:19:43 +0800

    people can't see edit button unless he is group owner

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :topics
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root "topics#index"
-  root "welcome#index"
+  # root "welcome#index"
+  root "groups#index"
 end
