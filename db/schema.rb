155fc00c557d34261012e346686363fe714c1166
zhuangtai <tai.zhuang@mvtrail.com>
2021-03-20 15:17:45 +0800

    create groups index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_124624) do
+ActiveRecord::Schema.define(version: 2021_03_20_071401) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
